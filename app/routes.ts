dd987f0e21436f374cc3e3ebb75211eb3e739bce
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-01-11 12:49:49 +0800

    added upload route

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import {index, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("/auth", "routes/auth.tsx"),
+    index("routes/home.tsx"),
+    route("/auth", "routes/auth.tsx"),
+    route("/upload", "routes/upload.tsx"),
 ] satisfies RouteConfig;
