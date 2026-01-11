72067783ee3bdcf20fde69ea68b9155967539362
Gerald Vincent Montibon <dev.montibon.geraldvincent54@gmail.com>
2026-01-11 11:20:55 +0800

    updated map to ResumeCard component; added padding to page-heading

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,22 +11,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    <Navbar />
-    <section className="main-section">
-      <div className="page-heading">
-        <h1>
-          Track Your Applications & Resume Ratings
-        </h1>
-        <h2>
-          Review your submissions and check AI-powered feedback
-        </h2>
-      </div>
-    </section>
-    {resumes.map((resume) => (
-        <div key={resume.id}>
-          <h1>{resume.jobTitle}</h1>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
         </div>
-    ))}
-  </main>;
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
